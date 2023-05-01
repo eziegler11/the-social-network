d923ea94f5e6ad06123932ff65f08e84b3cc389b
erichziegler <erich.ziegler11@gmail.com>
2023-05-01 14:15:27 -0600

    User routes completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,31 +1,77 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-  async getUsers(req, res) {
-    try {
-      const users = await User.find();
-      res.json(users);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
-  async getSingleUser(req, res) {
-    try {
-      const users = await User.findOne({_id: req.params.userId}).populate({path:"thoughts", select: "-_v"}).populate({path:"friends", select:"-_v"});
-      res.json(users);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
-  async createUser(req, res) {
-    try {
-      const users = User.create({
+	// Get All Users
+	async getUsers(req, res) {
+		try {
+			const users = await User.find();
+			res.json(users);
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
+
+	// Get A Single User
+	async getSingleUser(req, res) {
+		try {
+			const users = await User.findOne({ _id: req.params.userId })
+				.populate({ path: 'thoughts', select: '-_v' })
+				.populate({ path: 'friends', select: '-_v' });
+
+			if (!users) {
+				return res.status(404).json({ message: 'No user with that ID' });
+			}
+
+			res.json(users);
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
+
+	// Create A Single User
+	async createUser(req, res) {
+		try {
+			const users = User.create({
 				username: req.body.username,
 				email: req.body.email,
 			});
-      res.json(users);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  }
-}
+			res.json(users);
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
+
+	// Update A Single User
+	async updateUser(req, res) {
+		try {
+			const users = await User.findOneAndUpdate(
+				{ _id: req.params.userId },
+				{ $set: { username: req.body.username, email: req.body.email } },
+				{ runValidators: true, new: true }
+			);
+
+			// if (!users) {
+			// 	return res.status(404).json({ message: 'No user with that ID' });
+			// }
+
+			res.json(users);
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
+
+	// Delete A Single User
+	async deleteUser(req, res) {
+		try {
+			const users = await User.findOneAndDelete({ _id: req.params.userId });
+
+			// if (!users) {
+			// 	return res.status(404).json({ message: 'No user with that ID' });
+			// }
+
+			res.json(users);
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
+};
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,14 +3,17 @@ const {
 	getUsers,
 	getSingleUser,
 	createUser,
+	updateUser,
+	deleteUser
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers);
+router.route('/').post(createUser);
 
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser);
-router.route('/').post(createUser);
-//router.route('/:userId').put(updateSingleUser);
+router.route('/:userId').put(updateUser);
+router.route('/:userId').delete(deleteUser);
 
 module.exports = router;
